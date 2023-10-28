586b5c96c01b95b9ed20aa46537cb84878cc32fa
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-10-28 15:40:31 +0300

    ECOMMERCE

---
+++ middleware/full-auth.js
@@ -0,0 +1,39 @@
+const CustomError = require("../errors");
+const { isTokenValid } = require("../utils/jwt");
+
+const authenticateUser = async (req, res, next) => {
+  let token;
+  const authHeader = req.headers.authorization;
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    token = authHeader.split(" ")[1];
+  } else if (req.cookies.token) {
+    token = req.cookies.token;
+  }
+
+  if (!token) {
+    throw new CustomError.UnauthenticatedError("Authentication invalid");
+  }
+  try {
+    const payload = isTokenValid(token);
+    req.user = {
+      userId: payload.user.userId,
+      role: payload.user.role,
+    };
+    next();
+  } catch (error) {
+    throw new CustomError.UnauthenticatedError("Authentication invalid");
+  }
+};
+
+const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new CustomError.UnauthorizedError(
+        "Unauthorized to access this route"
+      );
+    }
+    next();
+  };
+};
+
+module.exports = { authenticateUser, authorizeRoles };
